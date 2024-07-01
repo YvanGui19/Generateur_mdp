7d90a1293e7c593e3d6670ad0947a1e6802f84fe
Yvan19 <dixneufcode@gmail.com>
2024-07-01 16:23:26 +0200

    Ajoutez des fichiers projet.

---
+++ Generateur_mdp/Outils.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//FormationCS.outils.DemanderNombrePositifNonNul
+
+namespace FormationCS
+{
+  static class Outils
+    {
+        public static int DemanderNombrePositifNonNul(string question)
+        {
+            return DemanderNombreEntre(question, 6, 23, "ERREUR : La longueur doit être comprise entre 6 et 23");
+        }
+
+        public static int DemanderNombreEntre(string question, int min, int max, string messageErreurPerso = null)
+        {
+            int nombre = DemanderNombre(question);
+            if ((nombre >= min) && (nombre <= max))
+            {
+                //valide
+                return nombre;
+            }
+            if (messageErreurPerso == null)
+            {
+                Console.WriteLine("ERREUR : Le nombre doit être compris entre " + min + " et " + max);
+                Console.WriteLine();
+            }
+            else
+            {
+                Console.WriteLine(messageErreurPerso);
+            }
+
+            return DemanderNombreEntre(question, min, max);
+        }
+
+        public static int DemanderNombre(string question)
+        {
+
+            while (true) //boucler tant qu'on a pas reçu une réponse valide (qui contient seulement des chiffres)
+            {
+                //poser la question
+                Console.Write(question);
+
+                //récupérer la réponse
+                string reponseLongueur = Console.ReadLine();
+
+                try
+                {
+                    //convertir
+                    int reponseInt = int.Parse(reponseLongueur);
+                    return reponseInt;
+                }
+                catch //gérer l'erreur de conversion
+                {
+                    Console.WriteLine("ERREUR : Renseigner un ou deux caractères numériques");
+                    Console.WriteLine();
+                }
+            }
+
+        }
+    }
+}
---
+++ Generateur_mdp/Program.cs
@@ -0,0 +1,59 @@
+﻿using FormationCS;
+using System;
+
+namespace Generateur_mdp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int combienMotDePasse = Outils.DemanderNombre(" Vous souhaitez générer combien de mot de passe ? ");
+            Console.WriteLine();
+
+            int longueurMotDePasse = Outils.DemanderNombrePositifNonNul(" Longueur du mot de passe : ");
+            Console.WriteLine();
+
+            int choixAlphabet = Outils.DemanderNombreEntre(" Vous voulez un mot de passe avec : \n" +
+                " 1 - Uniquement des caractères en minuscules\n" +
+                " 2 - Des caractères en minuscules et majuscules\n" +
+                " 3 - Des caractères minuscules, majuscules et des chiffres\n" +
+                " 4 - Des caractères minuscules, majuscules, des chiffres et des caractères spéciaux \n" +
+                "\n" +
+                " Votre choix (mettre seulement le numéro de ligne souhaité) : ", 1, 4);
+
+            for (int r = 0; r < combienMotDePasse; r++)
+            {
+                string minuscules = "abcdefghijklmnopqrstuvwxyz";
+                string majuscules = minuscules.ToUpper();
+                string chiffres = "0123456789";
+                string caracteresSpeciaux = "&~#$£*µ%§!?€|-_@.";
+                string alphabet;
+                string motDePasse = "";
+                Random rand = new();
+
+                if (choixAlphabet == 1)
+                    alphabet = minuscules;
+                else if (choixAlphabet == 2)
+                    alphabet = minuscules + majuscules;
+                else if (choixAlphabet == 3)
+                    alphabet = minuscules + majuscules + chiffres;
+                else
+                    alphabet = minuscules + majuscules + chiffres + caracteresSpeciaux;
+
+                int longueurAlphabet = alphabet.Length;
+
+                //boucle sur longueurMotDePasse
+                for (int i = 0; i < longueurMotDePasse; i++)
+                {
+                    int index = rand.Next(0, longueurAlphabet);
+                    motDePasse += alphabet[index]; //Génére le mot de passe
+                }
+                Console.WriteLine();
+                Console.WriteLine(" Mot de passe : " + motDePasse);
+            }
+            Console.WriteLine("\nAppuyez sur une touche pour fermer.");
+            Console.ReadKey();
+        }
+    }
+}
